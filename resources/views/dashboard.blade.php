dae030b4f6e5f471af551eaf46e48fcfb65853e4
pgt-2020-12-ilhamassegaf <62232296+pgt-2020-12-ilhamassegaf@users.noreply.github.com>
2020-05-01 19:29:17 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <section class="content-header">
       <h1>
-        DATA MAHASISWA
+        POLTEK GT
         <small>it all starts here</small>
       </h1>
       <ol class="breadcrumb">
@@ -20,7 +20,7 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+          <h3 class="box-title">Coding Class</h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
